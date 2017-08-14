2f7a437aee96e06be12170718f45adc151667b7a
Darnell Gillead <dlgillead@gmail.com>
2017-08-14 09:47:12 -0500

    completed the Article component

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class Article extends Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.created_date}</p>
+        <h2>{this.props.byline}</h2>
+        <img src={this.props.image} />
+        <p>{this.props.abstract}</p>
+      </div>
     )
   }
 }
